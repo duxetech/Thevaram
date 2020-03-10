8ebfa6e625620788b3d70d4a20d27a9ddd50c773
duxetech <karthikriches@gmail.com>
2020-03-10 20:57:06 +0530

    Initial Commit

---
+++ Thevaram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thevaram
+//
+//  Created by Karthik on 10/03/20.
+//  Copyright © 2020 Karthik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
